98047cb2dc889dc3eaec09c0bc21bff4925dadcc
sam <samuelerauti@gmail.com>
2023-12-15 16:30:58 +0100

    App language change implemented.

--- app/src/main/java/com/antolab/muscular/MainActivity.kt
+++ app/src/main/java/com/antolab/muscular/MainActivity.kt
@@ -7,6 +7,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import java.util.Calendar
+import androidx.appcompat.app.AlertDialog           //importazione della Classe: AlertDialog
+import java.util.*
+import android.app.Activity
+import android.content.Context                      //importazione della Classe: Context
+import android.content.res.Configuration            //importazione della Classe: Configuration
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,5 +58,69 @@ class MainActivity : AppCompatActivity() {
             60 * 1000, // Ogni tre secondi in millisecondi
             pendingIntent
         )
+
+        // Set up language change dialog
+        val langButton: Button = findViewById(R.id.bnLang)
+        langButton.setOnClickListener {
+            showChangeLang()
+        }
+
+        // Load saved language preference
+        loadLocate()
     }
+
+    private fun showChangeLang() {
+//crea un arraycon le lingue
+        val listItmes = arrayOf("English", "Español", "Français", "Italiano", "German")
+        //crea un AlertDialog
+        val mBuilder = AlertDialog.Builder(this@MainActivity)
+        mBuilder.setTitle(R.string.ad_title)
+        mBuilder.setSingleChoiceItems(listItmes, -1) { dialog, which ->
+            when (which) {
+                0 -> {
+                    setLocate("en") //English
+                    recreate()
+                }
+                1 -> {
+                    setLocate("es") //Español
+                    recreate()
+                }
+                2 -> {
+                    setLocate("fr") //Français
+                    recreate()
+                }
+                3 -> {
+                    setLocate("it") //Italiano
+                    recreate()
+                }
+                4 -> {
+                    setLocate("de") //Tedesco
+                    recreate()
+                }
+            }
+            dialog.dismiss()
+        }
+        val mDialog = mBuilder.create()
+        mDialog.show()
+    }
+
+    private fun setLocate(lang: String) {
+        val locale = Locale(lang)
+        Locale.setDefault(locale)
+        val config = Configuration()
+        config.locale = locale
+        baseContext.resources.updateConfiguration(config, baseContext.resources.displayMetrics)
+        val editor = getSharedPreferences("Settings", Context.MODE_PRIVATE).edit()
+        editor.putString("My_Lang", lang)
+        editor.apply()
+    }
+
+    private fun loadLocate() {
+        val sharedPreferences = getSharedPreferences("Settings", Activity.MODE_PRIVATE)
+        val language = sharedPreferences.getString("My_Lang", "")
+        if (language != "") {
+            setLocate(language!!)
+        }
+    }
+
 }
