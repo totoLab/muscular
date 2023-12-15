addd89486443d7bf15ca1f4ff5cba21c2a634aba
toto <antoniolabate19@gmail.com>
2023-12-15 18:22:16 +0100

    using database with old json to populate it for testing, removed unused methods

--- app/src/main/java/com/antolab/muscular/ExercisesActivity.kt
+++ app/src/main/java/com/antolab/muscular/ExercisesActivity.kt
@@ -8,47 +8,60 @@ import android.util.*
 import android.view.*
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
 import com.google.gson.reflect.TypeToken
 import java.io.*
 import java.util.*
-import android.Manifest
+import androidx.room.*
+import kotlinx.coroutines.*
+import com.antolab.muscular.db.*
 
 class ExercisesActivity : AppCompatActivity() {
     private val dbPath = "exercises.json"
-    private lateinit var exercisesDb : MutableMap<String, Exercise> // TODO needs to be unmutable, not var
-    private var permissionsGranted = false
-    private val MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 1
+    private lateinit var exerciseDatabase: ExerciseDatabase
+    private lateinit var exerciseDao: ExerciseDao
+    private var isDbEmpty = false
+    private val TESTING = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exercises)
         val scrollView = findViewById<ScrollView>(R.id.scrollView) ?: return // TODO remove unused
         val container = findViewById<LinearLayout>(R.id.container) ?: return
 
-        exercisesDb = readJsonFromFile(dbPath)
-        Log.d("recreationDB", "DB at activity creation: ${exercisesDb.toString()}")
+        try {
+            exerciseDatabase = Room.databaseBuilder(
+                applicationContext,
+                ExerciseDatabase::class.java,
+                "exercise_database"
+            ).build()
 
-        if (!checkWriteStoragePermission()) permissionsGranted = true
+            exerciseDao = exerciseDatabase.exerciseDao()
+            MainScope().launch {
+                if (TESTING && exerciseDao.getAllExercises().isEmpty()) prepopulation()
+            }
+        } catch (e: Exception) {
+            Log.e("RoomDatabase", "Error creating database", e)
+            isDbEmpty = true
+        }
 
-        // add exercises dynamically
-        if (exercisesDb.isEmpty()) {
+        if (isDbEmpty) {
             val empty = findViewById<TextView>(R.id.exercices_default_empty)
             empty.visibility = View.VISIBLE;
             return
         } else {
-            for (exerciseEntry in exercisesDb) {
-                val adding_outcome: Boolean = showExercise(container, exerciseEntry)
-                Log.d("exerciseLoading", "$exerciseEntry was ${if (adding_outcome) "" else "not"} added to the scroll view")
+            // add exercises dynamically
+            MainScope().launch {
+                for (exercise in exerciseDao.getAllExercises()) {
+                    val adding_outcome: Boolean = showExercise(container, exercise)
+                    Log.d("exerciseLoading", "$exercise was ${if (adding_outcome) "" else "not"} added to the scroll view")
+                }
             }
         }
     }
 
-    private fun showExercise(container: LinearLayout, entry: Map.Entry<String, Exercise>): Boolean {
-        val exerciseId = entry.key
-        val exercise = entry.value
+    private fun showExercise(container: LinearLayout, exercise: ExerciseEntity): Boolean {
         // Inflate the exercise template and make it visible
         val exerciseElement : RelativeLayout = layoutInflater.inflate(R.layout.exercise_template, null) as RelativeLayout
         exerciseElement.visibility = View.VISIBLE
@@ -69,14 +82,32 @@ class ExercisesActivity : AppCompatActivity() {
         val deleteButton = exerciseElement.findViewById<Button>(R.id.exercise_delete_button)
         deleteButton.setOnClickListener {
             container.removeView(exerciseElement)
-            exercisesDb.remove(exerciseId)
-            writeMapToJson(dbPath, exercisesDb)
-            Log.d("exerciseDeletion", "Deleted ${exercise.name} from the list. New Map:\n ${exercisesDb.toString()}")
+            MainScope().launch {
+                exerciseDao.delete(exercise)
+                Log.d("exerciseDeletion", "Deleted $exercise from the list.}")
+            }
         }
 
-        // add to the container
         container.addView(exerciseElement)
         return setImageOutcome
+    }
+
+    private suspend fun prepopulation() {
+        withContext(Dispatchers.IO) {
+            val oldDb : MutableMap<String, Exercise> = readJsonFromFile(dbPath)
+            Log.d("prepopulation", "json DB loading: ${oldDb.toString()}")
+            for (exerciseEntry in oldDb) {
+                var exercise = exerciseEntry.value
+                var exerciseEntity = ExerciseEntity(
+                    name = exercise.name,
+                    description = exercise.description,
+                    image = exercise.name
+                )
+                exerciseDao.insert(exerciseEntity)
+            }
+            val allExercises = exerciseDao.getAllExercises()
+            Log.d("prepopulation", allExercises.toString())
+        }
     }
 
     private fun readJsonFromFile(fileName: String): MutableMap<String, Exercise> {
@@ -162,44 +193,5 @@ class ExercisesActivity : AppCompatActivity() {
         @SerializedName("description") val description: String,
         @SerializedName("image") val image: String
     )
-
-    fun checkWriteStoragePermission(): Boolean {
-        // check if the permission was already granted
-        if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            // permission is not granted, request it
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE
-            )
-            return false // permission is not yet granted
-        } else {
-            return true // permission is already granted
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE -> {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // Permission granted, do nothing here, handle in the calling code
-                } else {
-                    // Permission denied, handle accordingly (e.g., show a message to the user)
-                }
-                // Call a method or set a flag to handle the result in the calling code
-            }
-            // Add other permission request cases if needed
-        }
-    }
 
 }
