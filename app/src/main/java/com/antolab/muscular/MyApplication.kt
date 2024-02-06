6c29c88849c2c789e3cf2772e3caaa955bfb6b82
toto <antoniolabate19@gmail.com>
2024-02-06 16:53:50 +0100

    dropped db migration support

--- app/src/main/java/com/antolab/muscular/MyApplication.kt
+++ app/src/main/java/com/antolab/muscular/MyApplication.kt
@@ -13,61 +13,6 @@ import com.antolab.muscular.db.AppDatabase
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
-val MIGRATION_1_2 = object : Migration(1, 2) {
-    override fun migrate(db: SupportSQLiteDatabase) {
-        // Check if the 'language' column does not exist before adding it
-        val cursor = db.compileStatement("PRAGMA table_info(exercises)").simpleQueryForLong()
-
-        // Find the column index for the 'language' column
-        val columnIndex = cursor.toInt()
-
-        if (columnIndex == -1) {
-            // 'language' column does not exist, add it
-            db.execSQL("ALTER TABLE exercises ADD COLUMN language TEXT NOT NULL DEFAULT 'it'")
-            // Add the new translation columns
-            db.execSQL("ALTER TABLE exercises ADD COLUMN name_en TEXT DEFAULT 'undefined'")
-            db.execSQL("ALTER TABLE exercises ADD COLUMN name_es TEXT DEFAULT 'undefined'")
-            db.execSQL("ALTER TABLE exercises ADD COLUMN name_fr TEXT DEFAULT 'undefined'")
-            db.execSQL("ALTER TABLE exercises ADD COLUMN name_de TEXT DEFAULT 'undefined'")
-            db.execSQL("ALTER TABLE exercises ADD COLUMN name_it TEXT DEFAULT 'undefined'")
-
-            db.execSQL("ALTER TABLE exercises ADD COLUMN description_es TEXT DEFAULT 'undefined'")
-            db.execSQL("ALTER TABLE exercises ADD COLUMN description_it TEXT DEFAULT 'undefined'")
-            db.execSQL("ALTER TABLE exercises ADD COLUMN description_en TEXT DEFAULT 'undefined'")
-            db.execSQL("ALTER TABLE exercises ADD COLUMN description_fr TEXT DEFAULT 'undefined'")
-            db.execSQL("ALTER TABLE exercises ADD COLUMN description_de TEXT DEFAULT 'undefined'")
-        }
-    }
-}
-
-val MIGRATION_2_3: Migration = object : Migration(2, 3) {
-    override fun migrate(db: SupportSQLiteDatabase) {
-        // Create a new table with the desired schema
-        db.execSQL("CREATE TABLE IF NOT EXISTS `programme_new` " +
-                "(`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
-                "`name` TEXT NOT NULL, " +
-                "`name_en` TEXT NOT NULL, " +
-                "`name_es` TEXT NOT NULL, " +
-                "`name_fr` TEXT NOT NULL, " +
-                "`name_de` TEXT NOT NULL, " +
-                "`name_it` TEXT NOT NULL)")
-
-        // Copy data from the old table to the new table
-        db.execSQL("INSERT INTO `programme_new` " +
-                "(`id`, `name`, `name_en`, `name_es`, `name_fr`, `name_de`, `name_it`) " +
-                "SELECT `id`, `name`, `name_en`, `name_es`, `name_fr`, `name_de`, `name_it` FROM `programme`")
-
-        // Remove the old table
-        db.execSQL("DROP TABLE `programme`")
-
-        // Rename the new table to the old table name
-        db.execSQL("ALTER TABLE `programme_new` RENAME TO `programme`")
-    }
-}
-
-
-
-
 class MyApplication : Application() {
 
     companion object {
@@ -89,8 +34,7 @@ class MyApplication : Application() {
                 applicationContext,
                 AppDatabase::class.java,
                 "app_database"
-            ).addMigrations(MIGRATION_1_2, MIGRATION_2_3)
-                .build()
+            ).build()
 
             appDao = appDatabase.appDao()
 
